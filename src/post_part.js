a68eac7736832db442b4f2cbfec930c45e306ba6
Jeremy Wright <3523018-chinchalupa@users.noreply.gitlab.com>
2020-01-13 07:04:25 +0000

    Add post parts to render different items
    - Post part can render text and youtube embedded links

---
+++ src/post_part.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class PostPart extends React.Component {
+    render() {
+        return ({
+            text: <p className="post-text">{this.props.content}</p>,
+            youtube: <iframe {...this.props} src={"https://www.youtube.com/embed/" + this.props.embed} frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        }[this.props.type])
+    }
+}
+
+export default PostPart
